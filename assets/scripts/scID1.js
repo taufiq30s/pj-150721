988ee46d1761618fd6b2f6475a6a982177236d9e
taufiq30s <35911547+taufiq30s@users.noreply.github.com>
2021-04-16 23:37:02 +0700

    Update scID1.js

--- assets/scripts/scID1.js
+++ assets/scripts/scID1.js
@@ -1,7 +1,6 @@
 monogatari.script({
   Indonesia: {
     'Start': [
-      "jump idPart2-2",
       "show scene galaxy with fadeIn",
       "Di sebuah Galaksi nan jauh disana, ada sebuah planet bernama Iolyptos.",
       "show scene iolyptos-city3 with fadeIn",
